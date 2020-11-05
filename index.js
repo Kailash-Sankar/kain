b7d7b12b41419eef22a9b03c5628fcff43ca417c
Saajan <saajan.sn@gmail.com>
2020-11-06 03:14:32 +0530

    screenshot routine complete and dontenv added

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+const serve = require("koa-static");
 const Koa = require("koa");
+const dotenv = require('dotenv');
+const path = require('path');
 const config = require("./config");
 const registerMiddlewares = require("./middlewares");
 const app = new Koa();
 const initScheduler = require("./scheduler");
 
+const ENV_FILE = path.join(__dirname, '.env');
+dotenv.config({ path: ENV_FILE });
+
+app.use(serve(__dirname + '/public'));
 // add essential middlewares
 registerMiddlewares(app);
 
