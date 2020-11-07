b0c54efaefa238ea51d49874c3f0892a75ff230c
Sindhu <sindhu@conviva.com>
2020-11-07 13:14:26 +0530

    added status to broadcasts

--- controllers/broadcasts/index.js
+++ controllers/broadcasts/index.js
@@ -2,7 +2,19 @@ const initRouter = require("../../utils/initRouter");
 const router = initRouter("broadcasts");
 
 router.get("/", async (ctx) => {
-  const broadcasts = await ctx.db.select().from("broadcasts");
+  let broadcasts = await ctx.db.select().from("broadcasts");
+  const queue = await ctx.db.select().from("scheduler_queue");
+  const statusObj = {
+    "-1": "Scheduled",
+    "2": "Delievered"
+  }
+  broadcasts = broadcasts.map(broadcast => {
+    const broadcastStatus = queue.find(item => (item.type_id == broadcast.id));
+    return {
+      ...broadcast,
+      status: statusObj[broadcastStatus.status]
+    }
+  });
   ctx.body = broadcasts;
 });
 
